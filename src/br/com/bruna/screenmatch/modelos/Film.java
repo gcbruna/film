10143b36727899f599e25653fcbb4a0edb7c1442
gcbruna <bruhbg55@gmail.com>
2024-04-12 17:16:43 -0300

    trocando os atributos e classes para publicos, pois estão em package diferentes

--- src/br/com/bruna/screenmatch/modelos/Film.java
+++ src/br/com/bruna/screenmatch/modelos/Film.java
@@ -1,28 +1,28 @@
 package br.com.bruna.screenmatch.modelos;
 
 public class Film {
-    String nome;
+    public String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
     private double somaDasAvaliacoes;  // Ele não deixa nem ler, nem escrever. Então o atributo privado não é somente leitura, ele está escondido, é necessário encontrar outra forma de acessá-lo.
     private int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
-    void exibeFichaTecnica(){    //método   não devolve nada, mas faz alguma coisa (não tem um retorno)
+    public void exibeFichaTecnica(){    //método   não devolve nada, mas faz alguma coisa (não tem um retorno)
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
-    void avalia(double nota){
+    public void avalia(double nota){
         somaDasAvaliacoes += nota;  //pega o que ja tinha na  avaliacao e incrementa e ir somando todas as avaliacoes
         totalDeAvaliacoes++;  //cada vez que colocar uma avaliação, ele ir incrementando essa minha total de avaliacoes
     }
 
-    double pegaMediaDasAvaliacoes(){
+    public double pegaMediaDasAvaliacoes(){
         return somaDasAvaliacoes / totalDeAvaliacoes;      //retorna
     }
 
-    int getTotalDeAvaliacoes(){    // retornasse e exibisse o total de avaliações, sem que seja possível alterá-lo. Isso é o que chamamos de "método acessor".
+    public int getTotalDeAvaliacoes(){    // retornasse e exibisse o total de avaliações, sem que seja possível alterá-lo. Isso é o que chamamos de "método acessor".
         return totalDeAvaliacoes;
     }
 }
