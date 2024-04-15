56910b1547b9093bed2bbac4bf96f9ba3d075b24
gcbruna <bruhbg55@gmail.com>
2024-04-15 18:27:00 -0300

    filme tem tudo o que Titulo tem e + o diretor

--- src/br/com/bruna/screenmatch/modelos/Film.java
+++ src/br/com/bruna/screenmatch/modelos/Film.java
@@ -1,44 +1,14 @@
 package br.com.bruna.screenmatch.modelos;
 
-public class Film {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;  // Ele não deixa nem ler, nem escrever. Então o atributo privado não é somente leitura, ele está escondido, é necessário encontrar outra forma de acessá-lo.
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Film extends Titulo{ // aplica herança á uma classe que no caso se diz o filme extende titulo
+    //filme tem tudo o que Titulo tem e + o diretor
+    private String diretor;
 
-    public void exibeFichaTecnica(){    //método   não devolve nada, mas faz alguma coisa (não tem um retorno)
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    public String getDiretor(){
+        return diretor;
     }
 
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;  //pega o que ja tinha na  avaliacao e incrementa e ir somando todas as avaliacoes
-        totalDeAvaliacoes++;  //cada vez que colocar uma avaliação, ele ir incrementando essa minha total de avaliacoes
-    }
-
-    public double pegaMediaDasAvaliacoes(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;      //retorna
-    }
-
-    public int getTotalDeAvaliacoes(){    // retornasse e exibisse o total de avaliações, sem que seja possível alterá-lo. Isso é o que chamamos de "método acessor".
-        return totalDeAvaliacoes;
-    }
-
-    public void setNome(String nome) {   // setter - deixar a pessoa incluir um nome
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
