a213a9a2fdf8280fa780a9540f1f50c3ca2e9582
gcbruna <bruhbg55@gmail.com>
2024-04-18 07:41:27 -0300

    implementando uma interface. Pois só pode herdar de uma única classe, mas pode implementar múltiplas interfaces.

--- src/br/com/bruna/screenmatch/modelos/Film.java
+++ src/br/com/bruna/screenmatch/modelos/Film.java
@@ -1,6 +1,8 @@
 package br.com.bruna.screenmatch.modelos;
 
-public class Film extends Titulo{ // aplica herança á uma classe que no caso se diz o filme extende titulo
+import br.com.bruna.screenmatch.calculos.Classificavel;
+
+public class Film extends Titulo implements Classificavel { // aplica herança á uma classe que no caso se diz o filme extende titulo
     //filme tem tudo o que Titulo tem e + o diretor
     private String diretor;
 
@@ -10,5 +12,10 @@ public class Film extends Titulo{ // aplica herança á uma classe que no caso s
 
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int) (pegaMediaDasAvaliacoes() / 2);  //sendo sobrescrito (substituído) da interface Classificavel.
     }
 }
