de6d7939b00e48e704a6812a9cad231adf94ba0b
gcbruna <bruhbg55@gmail.com>
2024-04-19 17:10:30 -0300

    mudando a instanciação padrão para a através do construtor com parâmetros

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import br.com.bruna.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.bruna.screenmatch.calculos.FiltroRecomendacao;
 import br.com.bruna.screenmatch.modelos.Episodios;
 import br.com.bruna.screenmatch.modelos.Film; //O importe é a forma que uma classe se referencia a outra quando estão em pacotes diferentes
-import br.com.bruna.screenmatch.modelos.Film;
 import br.com.bruna.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Film meuFilme = new Film(); //criando o espaço na memoria é o objeto
-        meuFilme.setNome("O Poderoso Chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Film meuFilme = new Film("O Poderoso Chefão", 1970); //criando o espaço na memoria é o objeto
         meuFilme.setDuracaoEmMinutos(190);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos()  + " minutos");
 
@@ -19,9 +19,7 @@ public class Main {
         System.out.println("Total de Avaliação: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMediaDasAvaliacoes());
 
-        Serie theLastOfUs = new Serie();
-        theLastOfUs.setNome("The Last Of Us");
-        theLastOfUs.setAnoDeLancamento(2023);
+        Serie theLastOfUs = new Serie("The Last Of Us", 2023);
         theLastOfUs.exibeFichaTecnica();
         theLastOfUs.setAtiva(true);
         theLastOfUs.setTemporadas(1);
@@ -29,30 +27,22 @@ public class Main {
         theLastOfUs.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar The Last Of Us: " + theLastOfUs.getDuracaoEmMinutos() + " minutos");
 
-        Film outroFilme = new Film();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
+        Film outroFilme = new Film("Avatar", 2023);
         outroFilme.setDuracaoEmMinutos(200);
         outroFilme.setIncluidoNoPlano(true);
 
 
-        Film favorito = new Film();
-        favorito.setNome("The Matrix");
-        favorito.setAnoDeLancamento(1999);
+        Film favorito = new Film("The Matrix", 1999);
         favorito.setDuracaoEmMinutos(135);
         favorito.setIncluidoNoPlano(true);
 
 
-        Film outro = new Film();
-        outro.setNome("John Wick");
-        outro.setAnoDeLancamento(2014);
+        Film outro = new Film("John Wick", 2014);
         outro.setDuracaoEmMinutos(101);
         outro.setIncluidoNoPlano(true);
 
 
-        Serie serie = new Serie();
-        serie.setNome("La Casa de Papel");
-        serie.setAnoDeLancamento(2017);
+        Serie serie = new Serie("La Casa de Papel", 2017);
         serie.setIncluidoNoPlano(true);
         serie.setAtiva(true);
         serie.setTemporadas(5);
@@ -80,6 +70,13 @@ public class Main {
 
         System.out.println("Tempo total: " + calculadora.getTempoTotal());
 
+        ArrayList<Film> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(favorito);
+        listaDeFilmes.add(outro);
+        listaDeFilmes.add(outroFilme);
 
+        System.out.println("Tamanho da lista: " +listaDeFilmes.size());
+        System.out.println("Primeiro Filme: " +listaDeFilmes.get(0));
+        System.out.println(listaDeFilmes);
     }
 }
